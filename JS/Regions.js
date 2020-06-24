fdec4ea5bf4a3676ad4bd0e71b7321aec5d86655
FARRUKH Nasim <farrukh.nasim1@studytafensw.edu.au>
2020-06-24 18:03:52 +1000

    did some refactoring about responsive

---
+++ JS/Regions.js
@@ -0,0 +1,25 @@
+var Regions = {
+    setup: function() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+        alert('hi there');
+    },
+
+    carousel: function() {
+        var i;
+        var myIndex = 0;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) { myIndex = 1 }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(3000);
+        alert('hi carousel');
+    }
+}
